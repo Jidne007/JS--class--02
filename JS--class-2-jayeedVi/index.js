5417b918a35346530245fc21ccc9828002dc0b92
Jidne007 <163674890+Jidne007@users.noreply.github.com>
2024-05-22 23:29:12 +0600

    Add files via upload

---
+++ JS--class-2-jayeedVi/index.js
@@ -0,0 +1,74 @@
+
+var firstVariable ='its our 1st variable';
+firstVariable='its changed';
+firstVariable=1;
+
+let secondVariable ='it is our 2nd Var';
+secondVariable ='its changed again,';
+
+const thirdVariable ='its our 3rd var';
+
+
+console.log(firstVariable,secondVariable,thirdVariable);
+
+
+// let number1=1;
+// let number2 = '2';
+
+// const add = number1 + number2;
+
+// console.log(add);
+// console.log(typeof(add));
+
+/**/
+
+let number1=1;
+let number2 = '2';
+
+const add = number1 - number2;
+
+console.log(add);
+console.log(typeof(add));
+
+/**/
+let firstLine = 'its you but not you';
+let secondline ='its our property';
+
+const fulline = firstLine + " " + secondline;
+
+console.log(fulline);
+console.log(typeof(fulline));
+
+
+/** */
+
+let number5 = +'3';
+let number6 = -'2';
+console.log(number5);
+console.log(typeof(number5));
+
+
+/** 
+
+let number7 = String(8);
+console.log(number7);
+console.log(typeof(number7));
+*/
+
+
+/*booleean type*/
+ 
+// let x = 1;
+// let y =2;
+// let z =3;
+
+// console.log(x < y, x==y, x!==y);
+
+/* parsefloat and parseint*/
+
+let a = 1.205454;
+let b = 2;
+let c = 3;
+
+console.log(parseFloat(a).toFixed(3));
+
